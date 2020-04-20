89f6cfc9c89572c51d4b3850823ef81a496e9922
Dev Bahl <devbahl12@yahoo.com>
2020-04-21 03:46:35 +0530

    resolved some errors

--- app/src/main/java/com/dbsrm/covid19tracker/MainActivity.kt
+++ app/src/main/java/com/dbsrm/covid19tracker/MainActivity.kt
@@ -1,17 +1,18 @@
 package com.dbsrm.covid19tracker
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import com.google.gson.Gson
+import androidx.appcompat.app.AppCompatActivity
 import com.google.gson.GsonBuilder
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.GroupieViewHolder
 import com.xwray.groupie.Item
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.data_row1.view.*
+import kotlinx.android.synthetic.main.main_data_card.view.*
 import okhttp3.*
 import java.io.IOException
+import java.util.*
 import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
@@ -20,26 +21,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        recyclerViewMain.adapter = adapter
         fetchJson()
-    }
-
-    class adddata(val details: Details): Item<GroupieViewHolder>(){
-
-        override fun getLayout(): Int {
-            return R.layout.data_row1
-        }
-
-        override fun bind(viewHolder: GroupieViewHolder, position: Int) {
-            viewHolder.itemView.dataView1.text = details.state.toString()
-        }
 
     }
-    val latestDataMap = HashMap<String, Details>()
-    val adapter = GroupAdapter<GroupieViewHolder>()
 
-
-    class Totaldata(): Item<GroupieViewHolder>(){
+    class AddData(val feed: Feed) : Item<GroupieViewHolder>() {
 
         override fun getLayout(): Int {
             return R.layout.main_data_card
@@ -47,13 +33,33 @@ class MainActivity : AppCompatActivity() {
 
         override fun bind(viewHolder: GroupieViewHolder, position: Int) {
 
+            val details = feed.statewise[0]
+            Log.d("data1", details.state)
+            viewHolder.itemView.total_cases.text = details.confirmed
+            viewHolder.itemView.active_cases.text = details.active
+            viewHolder.itemView.recovered_cases.text = details.recovered
+            viewHolder.itemView.deceased_cases.text = details.deaths
         }
 
     }
+
+    val adapter = GroupAdapter<GroupieViewHolder>()
+
+    /*  class Totaldata(): Item<GroupieViewHolder>(){
+
+        override fun getLayout(): Int {
+            return R.layout.data_row1
+        }
+
+        override fun bind(viewHolder: GroupieViewHolder, position: Int) {
+
+        }
+
+    }*/
     private fun fetchJson() {
         val url = "https://api.covid19india.org/data.json"
         val request = Request.Builder().url(url).build()
-        val client =  OkHttpClient.Builder()
+        val client = OkHttpClient.Builder()
             .connectTimeout(10, TimeUnit.SECONDS)
             .writeTimeout(10, TimeUnit.SECONDS)
             .readTimeout(30, TimeUnit.SECONDS)
@@ -67,19 +73,20 @@ class MainActivity : AppCompatActivity() {
                 val body = response.body?.string()
                 println(body)
                 val gson = GsonBuilder().create()
-                val feed = gson.fromJson(body,Feed::class.java)
+                val feed = gson.fromJson(body, Feed::class.java)
 
                 runOnUiThread {
-                    adapter.add(Totaldata())
-                    latestDataMap.values.forEach {
-                        adapter.add(adddata(it))
-                    }
+                    recyclerViewMain.adapter = adapter
+                    //  adapter.add(Totaldata())
+                    adapter.add(AddData(feed))
                 }
             }
         })
     }
 }
 
+
 class Feed(val statewise: List<Details>)
 
-class Details(val active:Int,val confirmed: Int, val deaths: Int,val lastupdatedtime: String,val recovered: Int,val state: String)
+class Details(val active:String, val confirmed: String, val deaths: String, val lastupdatedtime: String, val recovered: String, val state: String)
+
