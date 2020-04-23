d6d68b3048b7c50bd87c4d33cd91c7a5973f768e
Dev Bahl <devbahl12@yahoo.com>
2020-04-23 20:13:53 +0530

    ui changes

--- app/src/main/java/com/dbsrm/covid19tracker/Home.kt
+++ app/src/main/java/com/dbsrm/covid19tracker/Home.kt
@@ -5,11 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
+import com.xwray.groupie.Item
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.home.*
+import kotlinx.android.synthetic.main.recycler_home.*
 
 class Home: Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.home,container,false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
 }
